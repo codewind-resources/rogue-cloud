ee17b35ec95ec263d10beefede5c7a8ae33c6848
Jonathan West <jgwest@ca.ibm.com>
2018-06-19 10:02:53 -0400

    API mismatch message incorrectly shown for any connection exception

--- RogueCloudShared/src/com/roguecloud/utils/RCUtils.java
+++ RogueCloudShared/src/com/roguecloud/utils/RCUtils.java
@@ -22,6 +22,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
@@ -101,5 +103,12 @@ public class RCUtils {
 
 	public static void sleep(long msecs) {
 		try { Thread.sleep(msecs); } catch (InterruptedException e) { throw new RuntimeException(e); }
+	}
+	
+	public static String convertThrowableToString(Throwable t) {
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		t.printStackTrace(pw);
+		return sw.toString();
 	}
 }
